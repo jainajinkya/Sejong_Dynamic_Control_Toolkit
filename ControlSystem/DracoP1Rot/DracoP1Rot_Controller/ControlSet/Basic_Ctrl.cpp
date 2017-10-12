d433dc0761bde6477546518a06c59f487474ecff
dhkim0821 <dk6587@utexas.edu>
2017-10-12 13:30:26 -0500

    computation time test: using dynamic size matrix does not hurt performance when the size is large

--- ControlSystem/DracoP1Rot/DracoP1Rot_Controller/ControlSet/Basic_Ctrl.cpp
+++ ControlSystem/DracoP1Rot/DracoP1Rot_Controller/ControlSet/Basic_Ctrl.cpp
@@ -1,4 +1,5 @@
 #include "Basic_Ctrl.hpp"
+#include <chrono>
 
 Basic_Ctrl::Basic_Ctrl():DracoController(),
                          jpos_ini_(NUM_ACT_JOINT)
@@ -21,6 +22,8 @@ void Basic_Ctrl::ComputeTorqueCommand(sejong::Vector & gamma){
   _jpos_ctrl_hanging(gamma);
 
   _PostProcessing_Command(gamma);
+
+  // _time_test();
 }
 
 void Basic_Ctrl::_jpos_ctrl_hanging(sejong::Vector & gamma){
@@ -28,7 +31,7 @@ void Basic_Ctrl::_jpos_ctrl_hanging(sejong::Vector & gamma){
   double kd(2.);
   sejong::Vector qddot(NUM_QDOT); qddot.setZero();
 
-  jpos_des = jpos_ini_;
+  sejong::Vector jpos_des = jpos_ini_;
 
   for(int i(0);i<NUM_ACT_JOINT; ++i){
     qddot[i + NUM_VIRTUAL] = kp * (jpos_des[i] - sp_->Q_[i + NUM_VIRTUAL]) + kd * (-sp_->Qdot_[i + NUM_VIRTUAL]);
@@ -37,4 +40,67 @@ void Basic_Ctrl::_jpos_ctrl_hanging(sejong::Vector & gamma){
   sejong::Vector torque = A_ * qddot + grav_;
 
   gamma = torque.tail(NUM_ACT_JOINT);
+}
+
+void Basic_Ctrl::_time_test(){
+  int num_iter(2000);
+
+#define A1_row 100
+#define A1_col 50
+#define B1_col 20
+
+  Eigen::Matrix<double, A1_row, A1_col> A1;
+  Eigen::Matrix<double, A1_col, B1_col> B1;
+  Eigen::Matrix<double, A1_col, B1_col> B2;
+  Eigen::Matrix<double, A1_row, B1_col> result;
+  // A1.setZero(); B1.setZero(); result.setZero();
+  A1.setRandom();
+  B1.setRandom(); B2.setRandom();
+  result.setRandom();
+
+  Eigen::MatrixXd A1_dyn(A1_row, A1_col);
+  Eigen::MatrixXd B1_dyn(A1_col, B1_col);
+  Eigen::MatrixXd B2_dyn(A1_col, B1_col);
+  Eigen::MatrixXd result_dyn(A1_row, B1_col);
+  // A1_dyn.setZero(); B1_dyn.setZero(); result_dyn.setZero();
+  A1_dyn.setRandom();
+  B1_dyn.setRandom(); B2_dyn.setRandom();
+  result_dyn.setRandom();
+
+  std::chrono::high_resolution_clock::time_point t1;
+  std::chrono::high_resolution_clock::time_point t2;
+  std::chrono::duration<double> time_span_fixed;
+  std::chrono::duration<double> time_span_dyn;
+
+  t1 = std::chrono::high_resolution_clock::now();
+  for (int i(0); i<num_iter; ++i){
+    result = A1 * (B1 + B2);
+    result += result;
+
+    A1.setRandom();
+    B1.setRandom(); B2.setRandom();
+    result.setRandom();
+
+  }
+  t2 = std::chrono::high_resolution_clock::now();
+  time_span_fixed = std::chrono::duration_cast< std::chrono::duration<double> >(t2 - t1);
+
+
+  t1 = std::chrono::high_resolution_clock::now();
+  for (int i(0); i<num_iter; ++i){
+    result_dyn = A1_dyn * (B1_dyn + B2_dyn);
+    result_dyn += result_dyn;
+
+    A1_dyn.setRandom();
+    B1_dyn.setRandom(); B2_dyn.setRandom();
+    result_dyn.setRandom();
+
+  }
+  t2 = std::chrono::high_resolution_clock::now();
+
+  time_span_dyn = std::chrono::duration_cast< std::chrono::duration<double> >(t2 - t1);
+
+  std::cout << "All fixed size matrix: " << time_span_fixed.count()*1000.0 << "ms."<<std::endl;;
+  std::cout << "All dynamic size matrix: " << time_span_dyn.count()*1000.0 << "ms."<<std::endl;;
+
 }
